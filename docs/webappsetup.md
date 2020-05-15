9ff82d4f9a178ffc7361581c9de29c4b3aed0fab
Josh Terrill <joshterrill.dev@gmail.com>
2020-05-15 01:47:50 -0700

    added stubbed out documentation for system setup

---
+++ docs/webappsetup.md
@@ -0,0 +1,7 @@
+---
+id: webappsetup
+title: Web App Setup
+---
+
+This is a link to [another document.](doc3.md)  
+This is a link to an [external page.](http://www.example.com)
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,25 +21,25 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'T-Squared',
+  tagline: 'Documentation for T-Metrics automation suite',
+  url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 't-squared-documentation',
+  organizationName: 'tsquared',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'introduction', label: 'Docs'},
+    {href: '/api', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} T-Squared`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
